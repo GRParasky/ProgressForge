3f4c39f828b779601d9d32784828bfe7d062a916
Gabriel Rodrigo Parasky <grparasky@gmail.com>
2026-03-19 21:27:33 -0300

    feat(repository): Implementing Repository layer functions in TaskRepository

--- main/src/repository/TaskRepository.java
+++ main/src/repository/TaskRepository.java
@@ -1,4 +1,36 @@
 package repository;
 
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskRepository {
+
+    private List<Task> tasks;
+    private int nextId = 0;
+
+    public TaskRepository() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void createTask(String name){
+        this.tasks.add(new Task(this.nextId++, name));
+    }
+
+    public List<Task> findAll() {
+        return tasks;
+    }
+
+    public Task findById(int id){
+        return this.tasks.get(id-1);
+    }
+
+    public void updateTask(int id, String newName) {
+        this.tasks.get(id-1).setName(newName);
+    }
+
+    public void deleteTask(int id) {
+        this.tasks.remove(id-1);
+    }
 }
